60fbfd338b85f54752fedbf53b46fae60d36cfe5
pattichis <pattichis@gmail.com>
2023-02-07 12:48:34 -0700

    Trying to fix points.

--- lineart.py
+++ lineart.py
@@ -420,7 +420,7 @@ class cuteGraph:
           marker=dict(size=18,
                       line=dict(width=self.point_width,
                                 color=color)),
-          color=color,
+          marker_color=color,
           marker_symbol="x",
           name="Point ("+str(x_point)+" ,"+str(y_point)+")",
           mode="markers"))
